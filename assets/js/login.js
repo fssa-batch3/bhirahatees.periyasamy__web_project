0f590fc7c8de7e2f857428cb10b9ab1e431da65b
Bhirahateesvaran Periyasamy <118425824+geek-bhirahatees@users.noreply.github.com>
2023-02-07 15:04:35 +0530

    JS added

---
+++ assets/js/login.js
@@ -0,0 +1,7 @@
+// // import { writeFile } from "node:file-system";
+// let fs = require("node:fs");
+// let obj = [{ name: "bhirahatees" }];
+// let local = JSON.stringify(obj);
+// fs.writeFile("./data.json", local, (err) => {
+//   if (err) throw err;
+// });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,46 @@
 let imagePath = [];
 let elements = [];
+let json = [];
 let image_input = document.querySelector("#number");
+let selectedItem = [];
 let path = "";
-let i = 0;
+let local;
 
-// image_input.addEventListener("change",function(){
+// image_input.addEvent 3Listener("change",function(){
 //     console.log(image_input)
 // })
-document.getElementById("add").onclick = function () {
-  let link = window.prompt("Please Upload a link");
-  let cost = window.prompt("Add a Cost");
-  let ItemName = window.prompt("Add a name");
-  addItems(link, cost, ItemName);
-  i++;
-  // console.log(elements, i);
-  // dialogBox()
-  // console.log(image_input.value)
-};
-let inventory = document.getElementById("inventory");
-inventory.addEventListener("click", function () {
-  let item = document.querySelector(".items");
-  console.log(item.children());
-});
 
-function addItems(path, itemPrice, name) {
+// calling all funtion and creating a object
+
+function createCard() {
+  let ItemName = window.prompt("Add a name");
+  let link = window.prompt("Please Upload a link");
+  let quantity = window.prompt("How Many Items Do You Want To add ?");
+  let cost = window.prompt("Add a Cost");
+  if (ItemName !== "" && link !== "" && quantity !== NaN && cost !== NaN) {
+    let object = new Object();
+    object["name"] = ItemName;
+    object["quantity"] = parseInt(quantity);
+    object["cost"] = parseInt(cost);
+    object["image"] = link;
+    object["alt"] = ItemName;
+    json.push(object);
+    addItems(link, cost, ItemName, quantity);
+    elements.push(ItemName, quantity, cost);
+  }
+  console.log(json);
+  //  3 dialogBox();
+  // console.log(image_input.value);
+  i = 0;
+  window.localStorage.setItem("items", JSON.stringify(json));
+}
+//creating the container
+
+let i = 0;
+
+function addItems(path, itemPrice, name, quantity) {
   let row = document.createElement("div");
   row.classList.add("items");
-  row.setAttribute("id", `item${i}`);
   let element = document.getElementById("inventory");
   element.appendChild(row);
   let itemOfTheImage = image(path, name);
@@ -35,23 +49,38 @@ function addItems(path, itemPrice, name) {
   row.appendChild(nameOfItem);
   let custom_price = price(`₹${itemPrice}`);
   row.appendChild(custom_price);
-  let stockElement = stock();
+  let stockElement = stock(quantity);
   row.appendChild(stockElement);
   elements.push(row);
+  //eventlistner to find the which element we click
+  row.addEventListener("click", (e) => {
+    e.preventDefault();
+    let storageObj = new Object();
+    storageObj["name"] = name;
+    storageObj["quantity"] = i;
+    storageObj["price"] = itemPrice;
+    selectedItem.push(storageObj);
+    window.localStorage.setItem("select", JSON.stringify(stringData));
+  });
 }
 
+//creating dialog box (still progress)
+
 function dialogBox() {
-  let dialog = document.createElement("div");
+  let dialog = document.createElement("dialog");
   dialog.classList.add("dialog");
-  let inputbox = document.createElement("input");
-  inputbox.setAttribute("type", "file");
-  inputbox.setAttribute("accept", "image/png image/jpg");
-  inputbox.setAttribute("id", "add-image");
-  dialog.appendChild(inputbox);
-  // let pricInput = document.createElement()
+  // let inputbox = document.createElement("input");
+  // inputbox.setAttribute("type", "file");
+  // inputbox.setAttribute("accept", "image/png image/jpg");
+  // inputbox.setAttribute("id", "add-image");
+  // dialog.appendChild(inputbox);
+  // // let pricInput = document.createElement()
   let el = document.getElementById("main-container");
+  dialog.show();
   el.appendChild(dialog);
 }
+
+//adding price
 
 function price(priceItem) {
   let price = document.createElement("div");
@@ -68,6 +97,8 @@ function price(priceItem) {
   return price;
 }
 
+//adding name
+
 function itemName(str) {
   let nameEl = document.createElement("p");
   nameEl.classList.add("item-name");
@@ -76,16 +107,26 @@ function itemName(str) {
   return nameEl;
 }
 
-function stock() {
+//adding stock (want some changes)
+
+function stock(quantity) {
   let stockEl = document.createElement("div");
   stockEl.classList.add("stock");
   let instock = document.createElement("p");
-  instock.classList.add("instock");
-  let stockText = document.createTextNode("In Stock");
-  instock.appendChild(stockText);
+  if (quantity <= 0) {
+    instock.classList.add("outofstock");
+    let stockText = document.createTextNode("Out Of Stock");
+    instock.appendChild(stockText);
+  } else {
+    instock.classList.add("instock");
+    let stockText = document.createTextNode(`Stock left  ${quantity}`);
+    instock.appendChild(stockText);
+  }
   stockEl.appendChild(instock);
   return stockEl;
 }
+
+//adding image
 
 function image(path, alt) {
   let image = document.createElement("img");
@@ -94,6 +135,9 @@ function image(path, alt) {
   image.setAttribute("alt", alt);
   return image;
 }
+
+//adding price and quantity
+
 function priceAndquantity() {
   let price = document.createElement("span");
   let priceInput = document.createElement("input");
@@ -101,12 +145,16 @@ function priceAndquantity() {
   price.appendChild(priceInput);
   return price;
 }
-function excuter() {
-  for (let i = 0; i < 20; i++) {
+
+//excuting for customer page
+
+function excuter(object) {
+  for (let i = 0; i < 10; i++) {
     addItems(
-      "https://image.api.playstation.com/cdn/EP0001/CUSA09303_00/tzKcptCCUkiigpacybO8xWmvxPS7vIzk.png",
-      "1999",
-      "Assasins Creed Orgins (Golden Edition)"
+      object[i]["url"],
+      object[i]["price"],
+      object[i]["name"],
+      object[i]["quantity"]
     );
   }
 }
